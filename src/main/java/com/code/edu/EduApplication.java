b02995cf447f887c0a9104f1c1030c45b4d85a1d
CaoMengjiao <mengjiao.cao@teradata.com>
2018-06-20 23:09:23 +0800

    ip更新

--- src/main/java/com/code/edu/EduApplication.java
+++ src/main/java/com/code/edu/EduApplication.java
@@ -51,22 +51,22 @@ public class EduApplication extends SpringBootServletInitializer{
 	private Connector createSslConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
-		try {
-			File keystore = new ClassPathResource("springboot.jks").getFile();
+//		try {
+//			File keystore = new ClassPathResource(store_name).getFile();
             /*File truststore = new ClassPathResource("sample.jks").getFile();*/
 			connector.setScheme("https");
 			connector.setSecure(true);
 			connector.setPort(port);
 			protocol.setSSLEnabled(true);
-			protocol.setKeystoreFile(keystore.getAbsolutePath());
+//			protocol.setKeystoreFile(keystore.getAbsolutePath());
 			protocol.setKeystorePass(key_store_password);
 			protocol.setKeyPass(key_password);
 			return connector;
-		}
-		catch (IOException ex) {
-			throw new IllegalStateException("can't access keystore: [" + "keystore"
-					+ "] or truststore: [" + "keystore" + "]", ex);
-		}
+//		}
+//		catch (IOException ex) {
+//			throw new IllegalStateException("can't access keystore: [" + "keystore"
+//					+ "] or truststore: [" + "keystore" + "]", ex);
+//		}
 	}
 //    @Bean
 //    @LoadBalanced
