97f1d77ab5b65d07f062681fc3502eb146a51be0
CaoMengjiao <mengjiao.cao@teradata.com>
2018-07-20 17:19:59 +0800

    save

--- src/main/java/com/code/edu/config/WebSecurityConfig.java
+++ src/main/java/com/code/edu/config/WebSecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -58,7 +58,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .successHandler(loginSuccessHandler)
                 .failureHandler(loginFailedHandler)
-                .and().logout().permitAll()
+                .and().logout().deleteCookies("JSESSIONID")
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login").logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll()
                 .and().csrf().disable();
 //        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
 //        http.authorizeRequests()
